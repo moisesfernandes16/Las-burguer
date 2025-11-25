fefe87cf46b5a9346c091e277a9006e9acee7b9f
moisesfernades16 <moisesfernandes1105@gmail.com>
2025-11-25 18:33:22 -0300

    remove botão de fechar do modal de produto e limpa código desnecessário

--- script.js
+++ script.js
@@ -22,57 +22,9 @@ document.querySelectorAll('.produtos').forEach(produto => {
         modal.style.display = "block";
     });
 });
-
-fechar.addEventListener("click", () => {
-    modal.style.display = "none"
-})
-
 modal.addEventListener("click", (event) => {
     if (event.target === modal) {
         modal.style.display = "none";
-    }
-});
-function mostrarResultados(busca) {
-  resultados.innerHTML = '';
-
-  if (busca.length < 2) {
-    resultados.style.display = 'none';
-    return;
-  }
-
-  const achados = produtosInfo.filter(prod => 
-    (prod.nome && prod.nome.toLowerCase().includes(busca)) ||
-    (prod.descricao && prod.descricao.toLowerCase().includes(busca))
-  );
-
-  if (achados.length === 0) {
-    resultados.innerHTML = "<div>Nenhum produto encontrado</div>";
-    resultados.style.display = "block";
-    return;
-  }
-
-  achados.forEach(prod => {
-    const linha = document.createElement("div");
-    linha.innerHTML = `<strong>${prod.nome}</strong> 
-      <small style="opacity:.7;">${prod.descricao.slice(0, 50)}...</small>`;
-    linha.onclick = () => {
-      prod.botao.click();
-      resultados.style.display = "none";
-      barra.value = "";
-    };
-    resultados.appendChild(linha);
-  });
-  resultados.style.display = "block";
-}
-barra.addEventListener ("input", () =>{
-    mostrarResultados(barra.value.trim().toLowerCase());
-})
-barra.addEventListener("Keydown",(event) =>{ 
-    if (event.key === "Enter") {
-        const primeira = resultados.querySelector("div")
-        if(primeira){
-            primeira.click()
-        }
     }
 });
 document.addEventListener("click", (e) => {
